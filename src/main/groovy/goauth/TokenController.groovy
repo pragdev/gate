81ff6fbe530463b25c911024368757b7253de30a
Antonio Antonelli <me@antonioantonelli.com>
2016-10-26 13:48:23 +0100

    retrieving security service from the context

--- src/main/groovy/goauth/TokenController.groovy
+++ src/main/groovy/goauth/TokenController.groovy
@@ -4,6 +4,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory
 import groovy.json.JsonBuilder
 import groovy.util.logging.Log
 
+import javax.servlet.ServletConfig
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
@@ -12,16 +13,23 @@ import javax.servlet.http.HttpServletResponse
 @Log
 public class TokenController extends HttpServlet {
 
-  def security = new Security(credentialsRepository: new CredentialsRepository(datastore: DatastoreServiceFactory.datastoreService))
+  Security security
+
+  @Override
+  void init(ServletConfig config) throws ServletException {
+    super.init(config)
+
+    security = config.servletContext.getAttribute('security')
+  }
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     def credentials = new Credentials(request.getParameter('username'), request.getParameter('password'))
     def token = security.authenticate credentials
 
-    response.setHeader("Cache-Control", "no-store")
-    response.setHeader("Pragma", "no-cache")
+    response.setHeader('Cache-Control', 'no-store')
+    response.setHeader('Pragma', 'no-cache')
 
-    response.setContentType("application/json")
+    response.setContentType('application/json')
 
     def builder = new JsonBuilder()
     builder {
