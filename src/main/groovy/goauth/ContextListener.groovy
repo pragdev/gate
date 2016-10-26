93ac670eabc32120eb9e656045195944c6676b63
Antonio Antonelli <me@antonioantonelli.com>
2016-10-26 13:37:03 +0100

    added security service to the servlet context

---
+++ src/main/groovy/goauth/ContextListener.groovy
@@ -0,0 +1,28 @@
+package goauth
+
+import com.google.appengine.api.datastore.DatastoreServiceFactory
+import groovy.util.logging.Log
+
+import javax.servlet.ServletContextEvent
+import javax.servlet.ServletContextListener
+
+@Log
+class ContextListener implements ServletContextListener {
+
+  @Override
+  void contextInitialized(ServletContextEvent event) {
+    log.info 'Context initialized, loading services'
+
+    def datastoreService = DatastoreServiceFactory.datastoreService
+    def credentialsRepository = new CredentialsRepository(datastore: datastoreService)
+    def security = new Security(credentialsRepository: credentialsRepository)
+
+    event.servletContext.setAttribute('security', security)
+    log.info 'services loaded'
+
+  }
+
+  @Override
+  void contextDestroyed(ServletContextEvent event) {
+  }
+}
