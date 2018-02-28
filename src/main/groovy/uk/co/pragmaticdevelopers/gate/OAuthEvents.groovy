bb19680c62e1cb1dfc825940755e2561ee9d8383
Antonio Antonelli <antonio.antonelli@camelotglobal.com>
2018-02-28 13:41:04 +0000

    returning access token

--- src/main/groovy/uk/co/pragmaticdevelopers/gate/OAuthEvents.groovy
+++ src/main/groovy/uk/co/pragmaticdevelopers/gate/OAuthEvents.groovy
@@ -3,7 +3,7 @@ package uk.co.pragmaticdevelopers.gate
 interface OAuthEvents {
     void onAccessTokenIssued(AccessToken token, Credentials credentials)
     void onNewClientRegistered(Client client)
-    void onNewResourceOwner(ResourceOwner owner, AccessToken token)
+    AccessToken onNewResourceOwner(ResourceOwner owner)
     void onNewAccessRequest(AccessRequest accessRequest)
     void onGrantedAccess(storedAccessRequest)
 }
